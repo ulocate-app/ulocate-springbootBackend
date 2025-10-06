6faa5b0854308546bb1e8239b52ce867d973e8bd
Lattie <gracemszhao@gmail.com>
2025-10-05 22:15:43 -0400

    initialized spring boot setup w/ spring initializr. added plugins spring web, h2 database, and spring data jpa.

---
+++ ulocate-springbootBackend/src/main/java/com/ulocate/ulocatespringbootbackend/UlocateSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ulocate.ulocatespringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlocateSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlocateSpringbootBackendApplication.class, args);
+	}
+
+}
---
+++ ulocate-springbootBackend/src/test/java/com/ulocate/ulocatespringbootbackend/UlocateSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulocate.ulocatespringbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlocateSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
